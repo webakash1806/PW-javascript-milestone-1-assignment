d41e35af366f917fcbdda15ce4f8077af3fe8af1
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-03-22 19:04:25 +0530

    updated

---
+++ 2. Calculator/script.js
@@ -0,0 +1,46 @@
+let operator = "";
+
+function setOperator(id) {
+    operator = id;
+    console.log(operator);
+}
+
+function cls() {
+    document.getElementById("num1").value = ""
+    document.getElementById("num2").value = ""
+    document.getElementById("finres").innerHTML = ""
+}
+
+document.getElementById("multiplyBtn").innerHTML = "X"
+document.getElementById("divideBtn").innerHTML = "÷"
+document.getElementById("subtractBtn").innerHTML = "_"
+
+function res() {
+    let first = document.getElementById("num1").value;
+    console.log(first);
+
+    let second = document.getElementById("num2").value;
+    console.log(second);
+
+    let res = 0; // Initialize the result variable
+
+    switch (operator) {
+        case "multiplyBtn":
+            res = first * second;
+            break;
+        case "divideBtn":
+            res = first / second;
+            break;
+        case "addBtn":
+            res = parseInt(first) + parseInt(second); // Convert the strings to numbers before addition
+            break;
+        case "subtractBtn":
+            res = first - second;
+            break;
+        default:
+            alert("Invalid operator"); // Alert the user about invalid input
+            break;
+    }
+
+    document.getElementById("finres").innerHTML = `Result: ${res}`;
+}
