dda421bcb69ca084624c0765235ba238c97d8adf
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-03-22 18:41:55 +0530

    first commit

---
+++ 1. Password Validator/script.js
@@ -0,0 +1,32 @@
+function res() {
+    document.getElementById("password").value = ""
+    document.getElementById("confirmpassword").value = ""
+    document.getElementById("message").innerHTML = ""
+}
+
+
+
+function btn() {
+    let pas = document.getElementById("password").value
+    let confirm = document.getElementById("confirmpassword").value
+    let msg = document.getElementById("message")
+
+
+
+    if ((pas.length != 0) & (confirm.length != 0)) {
+        if (pas == confirm) {
+            msg.textContent = "Password Matched"
+            msg.style.color = "1A8F14"
+            console.log("Password Matched. Password Validation Successfull")
+        }
+        else {
+            msg.textContent = "Password Not Matched"
+            msg.style.color = "E41D1D"
+            console.log("Password Not Matched. Password Validation Unsuccessfull")
+        }
+    }
+    else {
+        msg.textContent = ""
+        alert("It should not be empty")
+    }
+}
