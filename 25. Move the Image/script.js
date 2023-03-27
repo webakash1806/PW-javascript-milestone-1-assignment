294bacdaaeec01931df563fb19d6db455b7c1edf
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-03-27 18:35:05 +0530

    updated

---
+++ 25. Move the Image/script.js
@@ -0,0 +1,20 @@
+function up() {
+    document.getElementById("img").style.top = "0"
+    document.getElementById("img").style.left = "38%"
+
+}
+
+function down() {
+    document.getElementById("img").style.top = "60vw"
+    document.getElementById("img").style.left = "38%"
+}
+
+function left() {
+    document.getElementById("img").style.left = "0"
+    document.getElementById("img").style.top = "30%"
+}
+
+function right() {
+    document.getElementById("img").style.left = "72vw"
+    document.getElementById("img").style.top = "30%"
+}
